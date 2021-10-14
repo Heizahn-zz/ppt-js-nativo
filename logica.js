83abf7990d7440ab9b27a034042d776aa75efe14
Humberto Bracho <hrbweibezahn@gmail.com>
2021-10-13 23:59:50 -0400

    final no tan final

--- logica.js
+++ logica.js
@@ -1,10 +1,10 @@
-const Piedra = 1;
-const Papel = 2;
-const Tijera = 3;
+const Piedra = 'rock';
+const Papel = 'paper';
+const Tijera = 'scissors';
 
-const Empate = 1;
-const Ganador = 2;
-const Perdedor = 3;
+const Empate = 0;
+const Ganador = 1;
+const Perdedor = 2;
 
 let estasJugando = false;
 let Score = 0
@@ -16,45 +16,88 @@ const resultadoPantalla = document.getElementById("puntosGanados")
 const main = document.getElementById("main");
 const seleccion = document.getElementById("seleccion")
 const resultadoVs = document.getElementById("resultadoVs");
+const reset = document.getElementById("reset");
+const leerReglas = document.getElementById("leerReglas");
+const reglas = document.getElementById("reglas");
+const cerrarReglas = document.getElementById("cerrarReglas");
+const userSeleccion = document.getElementById("userSeleccion");
+const machineSeleccion = document.getElementById("machineSeleccion")
 
-piedraBtn.addEventListener("click", function(){
+piedraBtn.addEventListener("click", function () {
   eleccion(Piedra);
-  console.log(Piedra)
   main.style.display = 'none';
   seleccion.style.display = 'flex';
 });
-papelBtn.addEventListener("click", function(){
+papelBtn.addEventListener("click", function () {
   eleccion(Papel);
-  console.log(Papel)
   main.style.display = 'none';
   seleccion.style.display = 'flex';
 });
-tijeraBtn.addEventListener("click", function(){
+tijeraBtn.addEventListener("click", function () {
   eleccion(Tijera);
-  console.log(Papel)
   main.style.display = 'none';
   seleccion.style.display = 'flex';
 })
+reset.addEventListener("click", () => {
+  seleccion.style.display = 'none';
+  main.style.display = 'flex';
+})
+leerReglas.addEventListener('click', function () {
+  main.style.display = 'none';
+  reglas.style.display = 'flex';
+})
+cerrarReglas.addEventListener('click', () => {
+  reglas.style.display = 'none';
+  seleccion.style.display = 'none';
+  main.style.display = 'flex';
 
-function eleccion(userOption){
-  const machineOption = Math.floor(Math.random() * 3) + 1;
-  const result = calcularResultado(userOption, machineOption);
+})
 
-  switch (result) {
-    case Empate:
-      (resultadoPantalla.innerHTML = Score) || (resultadoVs.innerHTML = "Fue un Empate");
-    break;
-    case Ganador:
-      (resultadoPantalla.innerHTML = Score += 1) && (resultadoVs.innerHTML = "Tu Ganastes");
-    break;
-    case Perdedor:
-      (resultadoPantalla.innerHTML = Score -= 1) && (resultadoVs.innerHTML = "Tu Perdistes");
-    break;
+function eleccion(userOption) {
+  userSeleccion.src = './img/' + userOption + '.svg';
+
+  resultadoVs.innerHTML = 'Maquina escogiendo';
+    const suspenso = setInterval(() => {
+    const machineOption = calcMachineOption();
+    machineSeleccion.src = './img/' + machineOption + '.svg';
+
+  }, 100);
+  setTimeout(function () {
+    clearInterval(suspenso);
+    const machineOption = calcMachineOption();
+    const result = calcularResultado(userOption, machineOption);
+
+    machineSeleccion.src = './img/' + machineOption + '.svg';
+
+    switch (result) {
+      case Empate:
+        ((resultadoPantalla.innerHTML = Score = Score) && (resultadoVs.innerHTML = 'Esto fue un Empate'));
+        break;
+      case Ganador:
+        ((resultadoPantalla.innerHTML = Score += 1) && (resultadoVs.innerHTML = 'Tu Ganastes'));
+        break;
+      case Perdedor:
+        ((resultadoPantalla.innerHTML = Score -= 1) && (resultadoVs.innerHTML = 'Tu Perdistes'));
+        break;
+    };
+  }, 500)
+}
+
+function calcMachineOption() {
+  const num = Math.floor(Math.random() * 3);
+
+  switch (num) {
+    case 0:
+      return Piedra;
+    case 1:
+      return Papel;
+    case 2:
+      return Tijera;
   }
 }
 
 function calcularResultado(userOption, machineOption) {
-  if (userOption === machineOption){
+  if (userOption === machineOption) {
     return Empate;
   } else if (userOption === Piedra) {
     if (machineOption === Papel) return Perdedor;
